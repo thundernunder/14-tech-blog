4a90bf71b76678b08e50fb57f7e3b46be8c5baf6
Nunder <nathannunderwood@gmail.com>
2022-10-12 16:16:28 -0700

    trying to fix syntax errors for Heroku deployment

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./user');
-const Blog = require('./blog');
-const Comment = require('./comment');
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
